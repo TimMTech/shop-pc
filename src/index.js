4cfa56cedf8f57a535646df7b718edb70babf87a
TimMTech <tmukhamedov97@gmail.com>
2022-04-09 18:58:08 -0400

    Basename added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/shop-pc">
       <App />
     </Router>
   </React.StrictMode>,
